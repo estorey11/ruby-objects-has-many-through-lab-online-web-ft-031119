54de2dabb2328cd75539fbbef2fb3a11883b3a1d
Eric Storey <estorey@gmail.com>
2019-03-28 02:20:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,8 +12,12 @@ class Doctor
   end
   
   def new_appointment(patient, date)
-    Appointment.new()
+    Appointment.new(date, patient, self)
   end
   
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor==self}
+  end
   
+  def patients
 end
